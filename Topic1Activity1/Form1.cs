ae7245a916191f360e37c7145c12e9ebc96dfb1c
edmora1 <101490526+edmora1@users.noreply.github.com>
2022-03-13 03:48:12 -0400

    Topic1Activity1 Visual Studio Files

---
+++ Topic1Activity1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic1Activity1
+{
+    public partial class Topic1Activity1 : Form
+    {
+        public Topic1Activity1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Excited to learn more about C#!"; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Go Lopes!"); 
+        }
+    }
+}
