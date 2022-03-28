86e4c42d55cbe2afaf5a9a80616acca761c3d19a
edmora1 <101490526+edmora1@users.noreply.github.com>
2022-03-27 23:51:07 -0400

    Topic 3 Activity 6
    Revised Activity Application

---
+++ Topic3Activity6/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Topic1Activity2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertButton = new System.Windows.Forms.Button();
+            this.userInputBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.convertButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.convertButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.convertButton.FlatAppearance.BorderSize = 3;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(280, 175);
+            this.convertButton.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(101, 27);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // userInputBox
+            // 
+            this.userInputBox.Location = new System.Drawing.Point(330, 57);
+            this.userInputBox.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.userInputBox.Name = "userInputBox";
+            this.userInputBox.Size = new System.Drawing.Size(132, 21);
+            this.userInputBox.TabIndex = 1;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(330, 108);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(132, 21);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input your Time Elapsed in Seconds:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Your time converted is: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(478, 248);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.userInputBox);
+            this.Controls.Add(this.convertButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Elapsed Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox userInputBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Topic3Activity6/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic1Activity2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double inputTime;
+                double outputTime;
+
+                inputTime = int.Parse(userInputBox.Text);
+
+                if (inputTime < 60)
+                {
+                    MessageBox.Show("Invalid entry, input time must be greater than 59 seconds.");
+                }
+
+                if (inputTime >= 60)
+                {
+                    outputTime = inputTime / 60;
+                    outputTextBox.Text = (outputTime.ToString("n3") + " minute/s");
+                }
+
+                if (inputTime >= 3600)
+                {
+                    outputTime = inputTime / 3600;
+                    outputTextBox.Text = (outputTime.ToString("n3") + " hour/s");
+                }
+
+                if (inputTime >= 86400)
+                {
+                    outputTime = inputTime / 86400;
+                    outputTextBox.Text = (outputTime.ToString("n3") + " day/s");
+                }
+            }
+            catch
+            {
+                //Display an error message. 
+                MessageBox.Show("Invalid data input for program."); 
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
