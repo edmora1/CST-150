a0de63098e3221f1cb0db6d1062c064c02aac75e
edmora1 <101490526+edmora1@users.noreply.github.com>
2022-04-03 22:38:29 -0400

    Activity 8 Files and Photo

---
+++ Activity 8/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Activity_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carbInput = new System.Windows.Forms.TextBox();
+            this.fatInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // carbInput
+            // 
+            this.carbInput.Location = new System.Drawing.Point(172, 41);
+            this.carbInput.Name = "carbInput";
+            this.carbInput.Size = new System.Drawing.Size(100, 20);
+            this.carbInput.TabIndex = 0;
+            // 
+            // fatInput
+            // 
+            this.fatInput.Location = new System.Drawing.Point(172, 10);
+            this.fatInput.Name = "fatInput";
+            this.fatInput.Size = new System.Drawing.Size(100, 20);
+            this.fatInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input grams of fat: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input grams of carbohydrates: ";
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(197, 86);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcBtn.TabIndex = 4;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 144);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fatInput);
+            this.Controls.Add(this.carbInput);
+            this.Name = "Form1";
+            this.Text = "CALORIES FROM FAT AND CARBS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox carbInput;
+        private System.Windows.Forms.TextBox fatInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calcBtn;
+    }
+}
+
---
+++ Activity 8/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            double fatCal, carbCal;
+            int userFat, userCarb;
+
+            userFat = int.Parse(fatInput.Text);
+            userCarb = int.Parse(carbInput.Text);
+
+            fatCal = FatCalories(userFat);
+            carbCal = CarbCalories(userCarb);
+
+            MessageBox.Show("Your calories from fat are: " + fatCal + "\n" + "Your calories from carbohydrates are: " + carbCal + "\n");
+        }
+            public double FatCalories(int gramsFat)
+            {
+                gramsFat = int.Parse(fatInput.Text);
+
+                return gramsFat * 9;
+
+            }
+            public double CarbCalories(int gramsCarbs)
+            {
+                gramsCarbs = int.Parse(carbInput.Text);
+
+                return gramsCarbs * 4;
+            }
+
+        
+    }
+}
