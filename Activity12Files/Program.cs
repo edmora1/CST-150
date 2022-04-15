84be99880484041bf37c3eef9dc064299df941c6
edmora1 <101490526+edmora1@users.noreply.github.com>
2022-04-15 03:32:22 -0400

    Act12 Files and Photo

---
+++ Activity12Files/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class LetterCount
+{
+    public static void Main()
+    {
+        int ltrCount = 0;
+        int i = 1; 
+
+       string inputFile = System.IO.File.ReadAllText("Input.txt");
+       
+        char[] chars = inputFile.ToCharArray();
+
+       
+        while(i < chars.Length) 
+       { 
+            if ((chars[i] == '.' || chars[i] == ' ' || chars[i] == ',' || chars[i] == '!' || chars[i] == '\n') && (chars[i-1] == 't' || chars[i -1] == 'e'))
+            {
+                ltrCount++;
+            }
+            i++;
+       }
+        Console.WriteLine("There are " + ltrCount + " words ending in t or e"); 
+    }
+}
+  
