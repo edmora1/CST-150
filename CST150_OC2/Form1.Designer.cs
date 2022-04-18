d82ff7b62c41cd63f6e5398be77c399f41343f57
edmora1 <101490526+edmora1@users.noreply.github.com>
2022-04-17 23:35:33 -0400

    CST150_OC2

---
+++ CST150_OC2/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Activity14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbShape = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbFill = new System.Windows.Forms.RadioButton();
+            this.rbOutline = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbName = new System.Windows.Forms.CheckBox();
+            this.cbDate = new System.Windows.Forms.CheckBox();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.pbFinal = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFinal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbShape
+            // 
+            this.lbShape.FormattingEnabled = true;
+            this.lbShape.Items.AddRange(new object[] {
+            "Circle",
+            "Triangle",
+            "Pentagon",
+            "Hexagon"});
+            this.lbShape.Location = new System.Drawing.Point(43, 57);
+            this.lbShape.Name = "lbShape";
+            this.lbShape.Size = new System.Drawing.Size(120, 95);
+            this.lbShape.TabIndex = 0;
+            this.lbShape.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a Shape";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbOutline);
+            this.groupBox1.Controls.Add(this.rbFill);
+            this.groupBox1.Location = new System.Drawing.Point(43, 190);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(120, 84);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select fill mode";
+            // 
+            // rbFill
+            // 
+            this.rbFill.AutoSize = true;
+            this.rbFill.Location = new System.Drawing.Point(7, 20);
+            this.rbFill.Name = "rbFill";
+            this.rbFill.Size = new System.Drawing.Size(37, 17);
+            this.rbFill.TabIndex = 0;
+            this.rbFill.TabStop = true;
+            this.rbFill.Text = "Fill";
+            this.rbFill.UseVisualStyleBackColor = true;
+            // 
+            // rbOutline
+            // 
+            this.rbOutline.AutoSize = true;
+            this.rbOutline.Location = new System.Drawing.Point(7, 44);
+            this.rbOutline.Name = "rbOutline";
+            this.rbOutline.Size = new System.Drawing.Size(58, 17);
+            this.rbOutline.TabIndex = 1;
+            this.rbOutline.TabStop = true;
+            this.rbOutline.Text = "Outline";
+            this.rbOutline.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbDate);
+            this.groupBox2.Controls.Add(this.cbName);
+            this.groupBox2.Location = new System.Drawing.Point(43, 311);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(120, 106);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Details";
+            // 
+            // cbName
+            // 
+            this.cbName.AutoSize = true;
+            this.cbName.Location = new System.Drawing.Point(7, 20);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(54, 17);
+            this.cbName.TabIndex = 0;
+            this.cbName.Text = "Name";
+            this.cbName.UseVisualStyleBackColor = true;
+            // 
+            // cbDate
+            // 
+            this.cbDate.AutoSize = true;
+            this.cbDate.Location = new System.Drawing.Point(7, 53);
+            this.cbDate.Name = "cbDate";
+            this.cbDate.Size = new System.Drawing.Size(49, 17);
+            this.cbDate.TabIndex = 1;
+            this.cbDate.Text = "Date";
+            this.cbDate.UseVisualStyleBackColor = true;
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "fillCircle.png");
+            this.imageList.Images.SetKeyName(1, "outlineCircle.png");
+            this.imageList.Images.SetKeyName(2, "fillTriangle.jpg");
+            this.imageList.Images.SetKeyName(3, "outlineTriangle.png");
+            this.imageList.Images.SetKeyName(4, "fillPentagon.png");
+            this.imageList.Images.SetKeyName(5, "outlinePentagon.png");
+            this.imageList.Images.SetKeyName(6, "fillHexagon.png");
+            this.imageList.Images.SetKeyName(7, "outlineHexagon.png");
+            // 
+            // pbFinal
+            // 
+            this.pbFinal.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pbFinal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFinal.Location = new System.Drawing.Point(230, 57);
+            this.pbFinal.Name = "pbFinal";
+            this.pbFinal.Size = new System.Drawing.Size(300, 300);
+            this.pbFinal.TabIndex = 4;
+            this.pbFinal.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.Location = new System.Drawing.Point(303, 364);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(158, 23);
+            this.labelName.TabIndex = 5;
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelName.Click += new System.EventHandler(this.labelName_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.Location = new System.Drawing.Point(306, 387);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(155, 23);
+            this.labelDate.TabIndex = 6;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(241, 442);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 7;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(344, 442);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(446, 442);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 502);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.pbFinal);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbShape);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shape Maker App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFinal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbShape;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbOutline;
+        private System.Windows.Forms.RadioButton rbFill;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbDate;
+        private System.Windows.Forms.CheckBox cbName;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.PictureBox pbFinal;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ CST150_OC2/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string shapeType = lbShape.SelectedItem.ToString();
+        }
+        private void labelName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            string shapeType = lbShape.SelectedItem.ToString();
+
+            if (shapeType == "Circle")
+            {
+                if (rbFill.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[0];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Circle";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Circle";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+                else if (rbOutline.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[1];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Circle";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Circle";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+            }
+            else if (shapeType == "Triangle")
+            {
+
+                if (rbFill.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[2];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Triangle";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Triangle";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+                else if (rbOutline.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[3];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Triangle";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Triangle";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+            }
+            else if (shapeType == "Pentagon")
+            {
+
+                if (rbFill.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[4];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Pentagon";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Pentagon";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+                else if (rbOutline.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[5];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Pentagon";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Pentagon";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+            }
+            else if(shapeType == "Hexagon")
+            {
+
+                if (rbFill.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[6];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Hexagon";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Hexagon";
+                        labelDate.Text = DateTime.Now.ToString();
+                    }
+                }
+                else if (rbOutline.Checked)
+                {
+                    pbFinal.Visible = true;
+                    pbFinal.Image = imageList.Images[7];
+                    if (cbName.Checked)
+                    {
+                        labelName.Text = "Hexagon";
+                        labelDate.Text = " "; 
+                    }
+                    if (cbDate.Checked)
+                    {
+                        labelDate.Text = DateTime.Now.ToString();
+                        labelName.Text = " "; 
+                    }
+                    if (cbName.Checked && cbDate.Checked)
+                    {
+                        labelName.Text = "Hexagon";
+                        labelDate.Text = DateTime.Now.ToString(); 
+                    }
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pbFinal.Image = null; 
+            pbFinal.Visible = false;
+            labelDate.Text = " "; labelName.Text = " "; 
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
